f2e8fed161e14864940eb7df1ca94a021b5b7d1a
evanquon35-alt <evanquon35@gmail.com>
2026-06-08 21:59:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Our story begins...");
+const sections = document.querySelectorAll(".memory");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("visible");
+        }
+    });
+},{
+    threshold:0.15
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
